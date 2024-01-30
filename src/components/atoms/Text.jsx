3153c599e2f48afd3468972069aa6f2ca418a133
Miguel Ángel Quinteros <maq.miguel.quinteros@gmail.com>
2024-01-30 07:43:04 -0300

    para seguir

---
+++ src/components/atoms/Text.jsx
@@ -0,0 +1,6 @@
+
+export const Text = ({variant, text}) => {
+    return (
+        <p>{text}</p>
+    )
+}
---
+++ src/components/organism/Card.jsx
@@ -0,0 +1,22 @@
+import { Icon } from "../atoms/Icon"
+import { Text } from "../atoms/Text"
+import { Title } from "../atoms/Title"
+
+export const Card = ({cd}) => {
+
+    return (
+        <section>
+            <div>
+                <Icon 
+                    type={cd.icon.type} />
+                <Title 
+                    variant={cd.title.variant}
+                    level={cd.title.level} 
+                    text={cd.title.text} />
+                <Text 
+                    variant={cd.text.variant} 
+                    text={cd.text.text} />
+            </div>
+        </section>
+    )
+}
