6e8bd7ec84f549ff4d9405f66e6f28b51da4bc49
Miguel Ángel Quinteros <maq.miguel.quinteros@gmail.com>
2024-06-07 18:55:10 -0300

    react

---
+++ src/components/atoms/Title.jsx
@@ -0,0 +1,26 @@
+import styled, {css} from "styled-components"
+
+const StyledH1 = styled.h1 `
+    color: red;
+    ${(props) => props.$variant === 'main' && css `
+        color: green;
+    `}
+`;
+
+export const Title = ({level, variant, children}) => {
+    return (
+        <>
+            {level === 'h1' && 
+                <StyledH1 $variant={variant}>{children}</StyledH1>}
+
+            {!level || level === 'h2' && 
+                <h2 $variant={variant}>{children}</h2>}
+
+            {level === 'h3' && 
+                <h3 $variant={variant}>{children}</h3>}
+                
+            {level === 'h4' && 
+                <h4 $variant={variant}>{children}</h4>}
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Title } from "../components/atoms/Title"
+
+export const Home = () => {
+  return (
+    <div>
+        <Title level={'h1'} variant={'main'}>
+          Hola nuevo mundo!!
+        </Title>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum soluta nisi expedita voluptatem suscipit deleniti error voluptas <span>quidem esse rerum debitis ipsa,</span> ipsum placeat mollitia quibusdam quae labore voluptates dolorum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis doloremque blanditiis nam, voluptatibus sequi, voluptatem <sub>reiciendis tempora nesciunt eveniet impedit distinctio quod</sub> quidem consectetur mollitia cupiditate illum! Consequatur, accusantium vero.</p>
+    </div>
+  )
+}
