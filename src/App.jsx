0f7244b723a00be7c634784b2324afbc69c1dfe4
Miguel Ángel Quinteros <57157602+maq-miguel-quinteros@users.noreply.github.com>
2024-11-13 15:26:17 +0000

    darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import HeaderLeft from "./pages/HeaderLeft"
 import Home from "./pages/Home"
 
 function App() {
 
+  const [darkMode, setDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode)
+    document.documentElement.classList.toggle('dark', !darkMode)
+  }
+
   return (
-    <div className="main-h-screen w-full flex flex-col sm:flex-row subpixel-antialiased bg-white">
-      <HeaderLeft />
+    <div className={`main-h-screen w-full flex flex-col sm:flex-row subpixel-antialiased 
+    ${darkMode?"bg-black":"bg-white"}`}>
+      <HeaderLeft darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Home />
     </div>
   )
--- src/pages/HeaderLeft.jsx
+++ src/pages/HeaderLeft.jsx
@@ -1,5 +1,33 @@
-export default function HeaderLeft() {
+import { BiMoon, BiSun } from 'react-icons/bi'
+import image from '/public/profile.png'
+
+export default function HeaderLeft({darkMode, toggleDarkMode}) {
+
     return (
-        <div className="min-h-screen flex-1">HeaderLeft</div>
+        <div className="
+        min-h-screen flex-1 relative p-10 transition-all duration-500
+        sm:border-r
+        dark:bg-black dark:text-white dark:border-gray-700">
+            
+            <div className="flex flex-col gap-5">
+                <div className="flex flex-col items-center justify-center gap-5">
+                    <img src={image} alt="profile" className='w-32 rounded-full' />
+                    <div className='text-center space-y-1'>
+                        <h2 className='text-4xl font-light '>Miguel <span className='font-semibold'>Quinteros</span></h2>
+                        <h4 className='text-xl font-light'>Desarrollador FullStack</h4>
+                    </div>
+                    <button className='absolute right-10 top-10' onClick={toggleDarkMode}>
+                        {
+                            darkMode ? (
+                                <BiSun className='text-2xl' />
+                            ) : (
+                                <BiMoon className='text-2xl' />
+                            )
+                        }
+                    </button>
+                </div>
+
+            </div>
+        </div>
     )
 }
